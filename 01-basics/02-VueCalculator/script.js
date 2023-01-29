61eac170b20d918a88433c7dfd68ef4be633e5a0
elmesheryakova <e.meshcheryakova@dairyculture.ru>
2023-01-29 13:49:45 +0300

    task 02-calculator

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,43 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+const vm = createApp({
+  data() {
+    return {
+      output: 0,
+      numberOne: null,
+      numberTwo: null
+    };
+  },
+  computed: {
+    sum () {
+      return this.numberOne + this.numberTwo
+    },
+    subtract () {
+      return this.numberOne - this.numberTwo
+    },
+    multiply () {
+      return this.numberOne * this.numberTwo
+    },
+    divide () {
+      return this.numberOne / this.numberTwo
+    }
+  },
+  methods: {
+    calc (operator) {
+      switch (operator) {
+        case 'sum':
+          this.output = this.sum
+          break
+        case 'subtract':
+          this.output = this.subtract
+          break
+        case 'multiply':
+          this.output = this.multiply
+          break
+        case 'divide':
+          this.output = this.divide
+          break
+      }
+    }
+  }
+}).mount('#app');
