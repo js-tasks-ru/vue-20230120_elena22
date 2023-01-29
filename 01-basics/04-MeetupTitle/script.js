31aaa1d31ea8de14ba482622f2d195467fff3d5f
elmesheryakova <e.meshcheryakova@dairyculture.ru>
2023-01-29 21:02:38 +0300

    task 04-meetupititle

--- 01-basics/04-MeetupTitle/script.js
+++ 01-basics/04-MeetupTitle/script.js
@@ -1,17 +1,33 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
 const API_URL = 'https://course-vue.javascript.ru/api';
 
-function fetchMeetupById(meetupId) {
-  return fetch(`${API_URL}/meetups/${meetupId}`).then((response) => {
-    if (response.ok) {
-      return response.json();
-    } else {
-      return response.json().then((error) => {
-        throw error;
+const vm = createApp({
+  data() {
+    return {
+      title: '',
+      id: undefined
+    };
+  },
+  watch: {
+    id (val) {
+      this.fetchMeetupById(val)
+    }
+  },
+  methods: {
+    fetchMeetupById(meetupId) {
+      return fetch(`${API_URL}/meetups/${meetupId}`).then((response) => {
+        if (response.ok) {
+          return response.json().then((res) => {
+            this.title = res.title
+          });;
+        } else {
+          return response.json().then((error) => {
+            throw error;
+          });
+        }
       });
     }
-  });
-}
+  }
+}).mount('#app');
 
-// Требуется создать Vue приложение
