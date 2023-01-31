f2b91f4f25767f0f72971f80cc359ceae3fdf1be
elmesheryakova <e.meshcheryakova@dairyculture.ru>
2023-02-01 01:08:01 +0300

    task 02/06-MeetupView

--- 02-components/06-MeetupView/components/MeetupView.js
+++ 02-components/06-MeetupView/components/MeetupView.js
@@ -1,13 +1,20 @@
 import { defineComponent } from '../vendor/vue.esm-browser.js';
 import UiAlert from './UiAlert.js';
 import UiContainer from './UiContainer.js';
-
+import MeetupCover from '../../03-MeetupCover/components/MeetupCover'
+import MeetupDescription from '../../02-MeetupDescription/components/MeetupDescription'
+import MeetupInfo from '../../04-MeetupInfo/components/MeetupInfo'
+import MeetupAgenda from '../../05-MeetupAgenda/components/MeetupAgenda'
 export default defineComponent({
   name: 'MeetupView',
 
   components: {
     UiAlert,
     UiContainer,
+    MeetupCover,
+    MeetupDescription,
+    MeetupInfo,
+    MeetupAgenda
   },
 
   props: {
@@ -19,20 +26,24 @@ export default defineComponent({
 
   template: `
     <div>
-      <!-- meetup cover-->
+      <MeetupCover :title="meetup.title" :image="meetup.image" />
 
       <UiContainer>
         <div class="meetup">
           <div class="meetup__content">
             <h3>Описание</h3>
-            <!-- meetup description -->
+            <MeetupDescription :description="meetup.description" />
 
             <h3>Программа</h3>
-            <!-- meetup agenda -->
-            <UiAlert>Программа пока пуста...</UiAlert>
+            <MeetupAgenda :agenda="meetup.agenda" v-if="meetup.agenda.length"/>
+            <UiAlert v-else>Программа пока пуста...</UiAlert>
           </div>
           <div class="meetup__aside">
-            <!-- meetup info -->
+            <MeetupInfo
+              :organizer="meetup.organizer"
+              :place="meetup.place"
+              :date="meetup.date"
+            />
           </div>
         </div>
       </UiContainer>
