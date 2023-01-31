f95bd8be159e604a1d66cf0b8069b3f8e1677d61
elmesheryakova <e.meshcheryakova@dairyculture.ru>
2023-02-01 00:57:46 +0300

    task 02/05-MeetupAgenda

--- 02-components/05-MeetupAgenda/components/MeetupAgenda.js
+++ 02-components/05-MeetupAgenda/components/MeetupAgenda.js
@@ -1,13 +1,19 @@
 import { defineComponent } from '../vendor/vue.esm-browser.js';
-// import MeetupAgendaItem from './MeetupAgendaItem.js';
+import MeetupAgendaItem from './MeetupAgendaItem.js';
 
 export default defineComponent({
   name: 'MeetupAgenda',
-
+  components: {MeetupAgendaItem},
+  props: {
+    agenda: {
+      type: Array,
+      require: true
+    }
+  },
   template: `
     <ul class="agenda">
-      <li class="agenda__item">
-        <!-- meetup agenda item -->
+      <li class="agenda__item" v-for="item in agenda" :key="item.id">
+        <meetup-agenda-item :agenda-item="item"/>
       </li>
     </ul>`,
 });
