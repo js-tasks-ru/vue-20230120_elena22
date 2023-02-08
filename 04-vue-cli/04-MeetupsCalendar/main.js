39dbbabb09ec09c39967766261a5b8e1913c7147
elmesheryakova <e.meshcheryakova@dairyculture.ru>
2023-02-08 13:34:09 +0300

    remove task-4

--- 04-vue-cli/04-MeetupsCalendar/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue';
-import App from './App';
-import '@/assets/styles/app.css';
-import '@/assets/styles/taskbook.css';
-import '@/assets/styles/_container.css';
-
-createApp(App).mount('#app');
