97159326c89bdd3ad2381fddac8fc545c28dcfa7
elmesheryakova <mesheryakova.elena90@gmail.com>
2023-03-13 21:09:59 +0300

    remove unnecessary constant

--- 13-reusability/01-ProgressPlugin/plugins/progress/index.js
+++ 13-reusability/01-ProgressPlugin/plugins/progress/index.js
@@ -2,7 +2,6 @@ import { createApp, inject } from 'vue';
 import TheTopProgressBar from './TheTopProgressBar.vue';
 // Используйте эту константу в качестве ключа provide/inject
 export const PROGRESS_KEY = Symbol('PROGRESS_KEY');
-export const ROUTER_KEY = Symbol('ROUTER_KEY');
 // Функция для удобного внедрения с Composition API
 export function useProgress() {
   return inject(PROGRESS_KEY);
